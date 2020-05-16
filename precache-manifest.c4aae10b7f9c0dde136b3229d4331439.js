cc79185c5ed20ad59129f2d19a765d5a320e9f6e
andrebrum01 <andrezinhobrun@gmail.com>
2020-05-15 23:37:39 -0400

    Updates

---
+++ precache-manifest.c4aae10b7f9c0dde136b3229d4331439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ab3356eeac5ae9a97593e9fabb6a312",
+    "url": "/sae/index.html"
+  },
+  {
+    "revision": "f611d53092c6863d7082",
+    "url": "/sae/static/css/main.7e08f384.chunk.css"
+  },
+  {
+    "revision": "01da261faa8b92a756d4",
+    "url": "/sae/static/js/2.95090ef6.chunk.js"
+  },
+  {
+    "revision": "8e03a6172a18937f604df845b9fb568c",
+    "url": "/sae/static/js/2.95090ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f611d53092c6863d7082",
+    "url": "/sae/static/js/main.4a69cb4f.chunk.js"
+  },
+  {
+    "revision": "0a73c849617aad586a6e",
+    "url": "/sae/static/js/runtime-main.7278ebba.js"
+  }
+]);
